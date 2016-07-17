38ba41f5bc1e35d8259e833e210c9104a9606e2c
Dmitry Yakimenko <detunized@gmail.com>
2016-07-17 18:22:26 +0200

    Fix publish again (fuck git)

--- publish.sh
+++ publish.sh
@@ -5,11 +5,13 @@ DIR=tmp
 
 rm -rf $DIR
 git clone --branch gh-pages . $DIR
-rsync -a --delete --exclude .git site/ $DIR/
+
 cd $DIR
-git remote set-url origin git@github.com:detunized/olgasoo.git
-git fetch
-git add -A
-git commit -m "Publish generated site"
-git push origin gh-pages
+    git remote set-url origin git@github.com:detunized/olgasoo.git
+    git pull --ff
+
+    rsync -a --delete --exclude .git ../site/ ./
+    git add -A
+    git commit -m "Publish generated site"
+    git push origin gh-pages
 cd ..

a230b8eef73efa1ff4dbb012d176da6162f12c50
Dmitry Yakimenko <detunized@gmail.com>
2016-07-17 21:15:41 +0200

    Image conversion script

---
+++ convert-images.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+
+require "fileutils"
+
+#FileUtils.rm_rf "images/galleries"
+
+images = Dir["images/original-galleries/**/*.jpg"]
+images.each do |src|
+    dst = src.sub "original-galleries", "galleries"
+    FileUtils.mkdir_p File.dirname dst
+
+    puts src
+    system "convert -resize 900x1000! #{src} #{dst}"
+end

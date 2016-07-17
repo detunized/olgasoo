38fd3b41b450b0cd168c9728ad8583cd25946e43
Dmitry Yakimenko <detunized@gmail.com>
2016-07-17 18:00:06 +0200

    Fetch before commiting

--- publish.sh
+++ publish.sh
@@ -7,8 +7,9 @@ rm -rf $DIR
 git clone --branch gh-pages . $DIR
 rsync -a --delete --exclude .git site/ $DIR/
 cd $DIR
+git remote set-url origin git@github.com:detunized/olgasoo.git
+git fetch
 git add -A
 git commit -m "Publish generated site"
-git remote set-url origin git@github.com:detunized/olgasoo.git
 git push origin gh-pages
 cd ..
